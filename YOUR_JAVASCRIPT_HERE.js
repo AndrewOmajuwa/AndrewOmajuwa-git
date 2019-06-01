9c971bf814e7bba0eaa18d08d1510c48b6fd9221
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-06-01 21:07:39 +0200

    Added section 3 and part of section 4

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,58 +1,82 @@
-// Write your JS here
+//Creating hero object
 const hero = {name: "Andrew",
-heroic: true,
-inventory: [],
-health: 10,
-weapon: {
-    type: "hi",
-    damage: 2
-},
-}
-
-const body = document.getElementsByTagName("body")
-const img = document.createElement("img");
-img.id = "inn";``
-body.appendChild(img)
-
-//reassign health property 
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "dagger",
+        damage: 2
+        },
+    };
+//Creating rest function 
 function rest (object){
+
+//Alert popUp
     if(object.health === 10){
         return alert("Hero at max health")
     };
+
+//reassigning Hero Health
     object.health = 10;
+
+//Returning object
     return object;    
 }
 
+//Creating pick-up-item function
 function pickUpItem(heroObj, weapObj){
-heroObj.inventory.push(weapObj)
-}
 
+//Adding weapon to inventory property array
+    heroObj.inventory.push(weapObj)
+};
 
+//Creating equip-weapon function
 function equipWeapon(herolikeobj){
+
+//reassigning weapon in inventory array
     if(herolikeobj.inventory.length > 0){
-herolikeobj.weapon = herolikeobj.inventory[0]}
-if(herolikeobj.inventory.length === 0){
+    herolikeobj.weapon = herolikeobj.inventory[0]
+};
+
+//preventing function log on empty inventory array
+    if(herolikeobj.inventory.length === 0){
    
-}
-}
+};
+};
 
-//creating onclick functions
+//creating onclick functions (restoreHealth, bagClick and Weapon log)
 
+//Creating restoreHealth onclick function
 function restoreHealth(){
- hero.health = 10   
-}
+    hero.health = 10   
+};
 
+//Creating weaponLog onclick function
 function weaponLog(){
-hero.inventory.push({
+    hero.inventory.push({
     type: "dagger",
     damage: 2
 });
-}
+};
 
-function reassignWeapon(){
-
-}
-
+//Creating bagClick onclick function
 function bagClick(){
-hero.weapon = hero.inventory[0]   
-}
+    hero.weapon = hero.inventory[0]   
+};
+
+//Creating displayStats function
+function displayStats (heroo){
+    const stats = Object.values(heroo)
+    return stats;
+};
+
+//Calling displayStats function
+displayStats(hero);
+
+
+//Create form Change-Hero-Name
+function changeHeroName(){
+    const newHeroName = document.getElementById("HeroName").value;
+    return newHeroName;
+};
+
